f324fc15d6520f99110abfdcd78000b48526f368
Gonzalo Flores (colab) <gfloresv0531@gmail.com>
2025-09-25 21:35:56 +0000

    feat(data) : Implementación de la Estrategia de Limpieza de Datos con Feature Flag USE_CLEAN_DATA

---
+++ data_cleaning.py
@@ -0,0 +1,7 @@
+# data_cleaning.py - Limpieza Basica con Feature Flag
+import pandas as pd
+def clean_data(use_clean: bool = False):
+  df = pd.read_csv("data/dataset.csv")
+  if use_clean:
+    df = df.dropna()
+  return df  
